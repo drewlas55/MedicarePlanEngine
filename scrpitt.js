9670b8a4743948213d3c219913ea5dd4fe803c27
Dominick Gurnari <31714879+dag157@users.noreply.github.com>
2018-02-03 17:44:19 -0500

    Add files via upload

--- scrpitt.js
+++ scrpitt.js
@@ -1,8 +1,12 @@
 function Validate() {
                 //retreive data
-                var vName = document.getElementById("medications");
-                
-                var location = document.getElementById("location");
+    var input1 = document.getElementById("input4");
+    var input2 = document.getElementById("input4");
+               var input3 = document.getElementById("input4");
     
-                var conditions = document.getElementById("myInput");
-            }
+                var input4 = document.getElementById("input4");
+    
+    document.getElementById("search").innerHTML = input1.value + " " + input2.value + " " + input4.value;
+
+
+}
