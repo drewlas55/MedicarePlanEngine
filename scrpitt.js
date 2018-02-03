cad358698af3e2b07cd9d87f373090319e95962e
Dominick Gurnari <31714879+dag157@users.noreply.github.com>
2018-02-03 15:02:46 -0500

    created js file and imported it into index file

---
+++ scrpitt.js
@@ -0,0 +1,8 @@
+function Validate() {
+                //retreive data
+                var vName = document.getElementById("medications");
+                
+                var location = document.getElementById("location");
+    
+                var conditions = document.getElementById("myInput");
+            }
