b5c90efde64b19b483a5320525a2f9815d4daa73
Dominick Gurnari <31714879+dag157@users.noreply.github.com>
2018-02-03 17:55:07 -0500

    Some stuff

--- scrpitt.js
+++ scrpitt.js
@@ -1,12 +1,12 @@
 function Validate() {
                 //retreive data
-    var input1 = document.getElementById("input4");
-    var input2 = document.getElementById("input4");
-               var input3 = document.getElementById("input4");
+    var input1 = document.getElementById("input1");
+    var input2 = document.getElementById("input2");
+               var input3 = document.getElementById("input3");
     
                 var input4 = document.getElementById("input4");
     
-    document.getElementById("search").innerHTML = input1.value + " " + input2.value + " " + input4.value;
+    document.getElementById("search").innerHTML = input1.value;
 
 
 }
